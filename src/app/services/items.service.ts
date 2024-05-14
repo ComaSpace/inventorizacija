1099b3effcab515753e0e33a7387401eda210b5e
ComaSpace <kijucrolandas@gmail.com>
2024-05-14 12:54:58 +0300

    Async Validator

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,23 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Item } from '../models/item';
-import { map } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  constructor(private http:HttpClient) {
+  constructor(private http:HttpClient) { 
 
-   }
-
-    public addItem(item:Item){
-    return this.http.post('https://inventorizacija-e9221-default-rtdb.europe-west1.firebasedatabase.app/items.json',item);
   }
 
-public loadItems(){
-    return this.http.get<{[key:string]:Item}>('https://inventorizacija-d9f77-default-rtdb.europe-west1.firebasedatabase.app/items.json')
+  
+
+
+  public addItem(item:Item){
+    return this.http.post('https://inventorizacija-d9f77-default-rtdb.europe-west1.firebasedatabase.app/items.json',item);
+  }
+
+  public loadItems(){
+    return this.http.get<{[key:string]:Item}>('/items.json')
     .pipe(
       map((data):Item[]=>{
         let emp:Item[]=[];
@@ -29,5 +33,40 @@ public loadItems(){
       })
     )
   }
+
+ public static createUniqueInvNumberValidator(itemsService:ItemsService):AsyncValidatorFn{
+  return  (control: AbstractControl):  Observable<ValidationErrors | null> => {
+    return itemsService.loadItems().pipe(
+      map((data)=>{
+        let error=false;
+        data.forEach((v,k)=>{
+          if (control.value==v.inv_number){
+            error=true;
+          }
+        });
+        if (error){
+          return {"error":"Toks numeris jau egzistuoja"};
+        }else{
+          return null;
+        }
+        
+      })
+    );
+  }
+
+ }
+
+ public getLastInvNumber():Observable<number>{
+  return this.loadItems().pipe(
+    map((data)=>{
+      let n=0;
+      data.forEach(v=>{
+        if (v.inv_number!=null && v.inv_number>n) n=v.inv_number;
+      });
+      return Number(n)+1;
+
+    })
+  );
+ }
 
 }
